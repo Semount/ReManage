5f8efe9b56a7354bb5776b97e7451ae0e517b7d7
Semount <semount228@yandex.ru>
2024-05-14 00:53:53 +0300

    Order creation is working properly now + added "cart" for added dishes to an order

--- ReManage/Models/OrderedDishModel.cs
+++ ReManage/Models/OrderedDishModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using ReManage.Core;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ReManage.Models
 {
     [Table("ordered_dishes")]
-    public class OrderedDishModel : ViewModelBase
+    public class OrderedDishModel : ViewModelBase, INotifyPropertyChanged
     {
         [Column("id")]
         public int id { get; set; }
@@ -15,7 +17,34 @@ namespace ReManage.Models
         [Column("dish_id")]
         public int dish_id { get; set; }
 
+        private int _amount;
         [Column("amount")]
-        public int amount { get; set; }
+        public int amount
+        {
+            get => _amount;
+            set
+            {
+                if (_amount != value)
+                {
+                    _amount = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string _dishName;
+        [NotMapped]
+        public string DishName
+        {
+            get => _dishName;
+            set => SetProperty(ref _dishName, value);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- ReManage/ViewModels/OrderCreationViewModel.cs
+++ ReManage/ViewModels/OrderCreationViewModel.cs
@@ -18,6 +18,8 @@ public class OrderCreationViewModel : ViewModelBase
     private ObservableCollection<DishModel> _dishes;
     private ObservableCollection<CategoryModel> _categories;
     private ObservableCollection<DishModel> _selectedDishes;
+    private ObservableCollection<OrderedDishModel> _orderedDishes;
+    private OrderedDishModel _selectedOrderedDish;
     private string _comment;
 
     public ObservableCollection<DishModel> Dishes
@@ -36,6 +38,18 @@ public class OrderCreationViewModel : ViewModelBase
     {
         get => _selectedDishes;
         set => SetProperty(ref _selectedDishes, value);
+    }
+
+    public ObservableCollection<OrderedDishModel> OrderedDishes
+    {
+        get => _orderedDishes;
+        set => SetProperty(ref _orderedDishes, value);
+    }
+
+    public OrderedDishModel SelectedOrderedDish
+    {
+        get => _selectedOrderedDish;
+        set => SetProperty(ref _selectedOrderedDish, value);
     }
 
     public CategoryModel SelectedCategory
@@ -59,6 +73,9 @@ public class OrderCreationViewModel : ViewModelBase
     public ICommand AddDishCommand { get; }
     public ICommand PlaceOrderCommand { get; }
     public ICommand ShowAllDishesCommand { get; }
+    public ICommand IncrementOrderedDishCommand { get; }
+    public ICommand DecrementOrderedDishCommand { get; }
+    public ICommand RemoveOrderedDishCommand { get; }
 
     public OrderCreationViewModel(int employeeId, int tableNumber)
     {
@@ -66,10 +83,14 @@ public class OrderCreationViewModel : ViewModelBase
         _tableNumber = tableNumber;
         _context = new RestaurantContext();
         _selectedDishes = new ObservableCollection<DishModel>();
+        _orderedDishes = new ObservableCollection<OrderedDishModel>();
 
         AddDishCommand = new RelayCommand(AddDish);
         PlaceOrderCommand = new RelayCommand(PlaceOrder);
         ShowAllDishesCommand = new RelayCommand(_ => LoadAllDishes());
+        IncrementOrderedDishCommand = new RelayCommand(IncrementOrderedDish);
+        DecrementOrderedDishCommand = new RelayCommand(DecrementOrderedDish);
+        RemoveOrderedDishCommand = new RelayCommand(RemoveOrderedDish);
 
         LoadCategories();
         LoadAllDishes();
@@ -125,45 +146,108 @@ public class OrderCreationViewModel : ViewModelBase
     {
         if (parameter is DishModel dish)
         {
-            SelectedDishes.Add(dish);
+            var orderedDish = OrderedDishes.FirstOrDefault(od => od.dish_id == dish.Id);
+            if (orderedDish != null)
+            {
+                orderedDish.amount++;
+            }
+            else
+            {
+                OrderedDishes.Add(new OrderedDishModel
+                {
+                    dish_id = dish.Id,
+                    DishName = dish.Name,
+                    amount = 1
+                });
+            }
+
+            // Убедимся, что блюдо также добавлено в SelectedDishes
+            if (!SelectedDishes.Contains(dish))
+            {
+                SelectedDishes.Add(dish);
+            }
+        }
+    }
+
+    private void IncrementOrderedDish(object parameter)
+    {
+        if (parameter is OrderedDishModel orderedDish)
+        {
+            orderedDish.amount++;
+        }
+    }
+
+    private void DecrementOrderedDish(object parameter)
+    {
+        if (parameter is OrderedDishModel orderedDish && orderedDish.amount > 1)
+        {
+            orderedDish.amount--;
+        }
+    }
+
+    private void RemoveOrderedDish(object parameter)
+    {
+        if (parameter is OrderedDishModel orderedDish)
+        {
+            OrderedDishes.Remove(orderedDish);
+
+            // Также удалим блюдо из SelectedDishes, если оно больше не заказано
+            var correspondingDish = SelectedDishes.FirstOrDefault(d => d.Id == orderedDish.dish_id);
+            if (correspondingDish != null)
+            {
+                SelectedDishes.Remove(correspondingDish);
+            }
         }
     }
 
     private void PlaceOrder(object parameter)
     {
-        decimal totalPrice = SelectedDishes.Sum(d => d.Price);
-        NodaTime.Duration maxCookingTime = NodaTime.Duration.FromMinutes(SelectedDishes.Max(d => d.CookingTime.HasValue ? d.CookingTime.Value.TotalMinutes : 0));
-
-        OrderModel newOrder = new OrderModel
+        if (OrderedDishes.Any())
         {
-            employee_id = _employeeId,
-            status_id = 1, // Идентификатор статуса "Готовится"
-            price = totalPrice,
-            creation_date = DateTime.UtcNow.Date,
-            creation_time = DateTime.UtcNow.TimeOfDay,
-            serving_time = DateTime.UtcNow.TimeOfDay.Add(maxCookingTime.ToTimeSpan()),
-            comment = Comment
-        };
+            decimal totalPrice = OrderedDishes.Sum(od => od.amount * SelectedDishes.FirstOrDefault(d => d.Id == od.dish_id)?.Price ?? 0);
+            NodaTime.Duration maxCookingTime = NodaTime.Duration.FromMinutes(OrderedDishes.Max(od => {
+                var dish = SelectedDishes.FirstOrDefault(d => d.Id == od.dish_id);
+                if (dish?.CookingTime.HasValue ?? false)
+                {
+                    return (long)dish.CookingTime.Value.TotalMinutes;
+                }
+                return 0; // Возвращаем 0, если время готовки не определено
+            }));
 
-        newOrder.SetCreationDateToUtc(); // Преобразуем дату в формат UTC
-
-        _context.Orders.Add(newOrder);
-        _context.SaveChanges();
-
-        foreach (var dish in SelectedDishes.GroupBy(d => d.Id).Select(g => new { DishId = g.Key, Amount = g.Count() }))
-        {
-            OrderedDishModel orderedDish = new OrderedDishModel
+            OrderModel newOrder = new OrderModel
             {
-                // id поле не устанавливается, оно будет сгенерировано автоматически
-                order_id = newOrder.id,
-                dish_id = dish.DishId,
-                amount = dish.Amount
+                employee_id = _employeeId,
+                status_id = 1,
+                price = totalPrice,
+                creation_date = DateTime.UtcNow.Date,
+                creation_time = DateTime.UtcNow.TimeOfDay,
+                serving_time = DateTime.UtcNow.TimeOfDay.Add(maxCookingTime.ToTimeSpan()),
+                comment = Comment
             };
-            _context.OrderedDishes.Add(orderedDish);
+
+            newOrder.SetCreationDateToUtc();
+
+            _context.Orders.Add(newOrder);
+            _context.SaveChanges();
+
+            foreach (var dish in OrderedDishes)
+            {
+                OrderedDishModel orderedDish = new OrderedDishModel
+                {
+                    order_id = newOrder.id,
+                    dish_id = dish.dish_id,
+                    amount = dish.amount
+                };
+                _context.OrderedDishes.Add(orderedDish);
+            }
+
+            _context.SaveChanges();
+            MessageBox.Show("Заказ успешно создан!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
         }
-
-        _context.SaveChanges();
-        MessageBox.Show("Заказ успешно создан!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        else
+        {
+            MessageBox.Show("Не выбрано ни одно блюдо!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
     }
-
 }
+
